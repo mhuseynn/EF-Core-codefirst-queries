69ae9833aab12e8dafae7f9cdc1230a17586d9ee
mhuseynn <hmehdiyev2005@gmail.com>
2024-02-14 19:06:34 +0400

    task

---
+++ DebtorPract/Contexts/DebtorDb.cs
@@ -0,0 +1,33 @@
+﻿using DebtorPract.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebtorPract.Contexts
+{
+    internal class DebtorDb : DbContext
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var ConStr = "Initial Catalog=DebtorDb;Data Source=DESKTOP-R8Q6LD3\\SQLEXPRESS;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+
+            optionsBuilder
+                .UseLazyLoadingProxies()
+                .UseSqlServer(ConStr);
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public virtual DbSet<Debtor>? Debtors { get; set; }
+    }
+}
---
+++ DebtorPract/Entities/Debtor.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebtorPract.Entities
+{
+    public class Debtor
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public DateTime BirthDay { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public int Debt { get; set; }
+
+        public Debtor()
+        {
+            Id = 0;
+            this.FullName = "";
+            this.BirthDay = DateTime.Now;
+            this.Phone = "";
+            this.Email = "";
+            this.Address = "";
+            this.Debt = 0;
+        }
+
+        public Debtor(string fullname, DateTime birthDay, string phone, string email, string address, int debt)
+        {
+            
+            this.FullName = fullname;
+            this.BirthDay = birthDay;
+            this.Phone = phone;
+            this.Email = email;
+            this.Address = address;
+            this.Debt = debt;
+        }
+
+
+
+        public override string ToString()
+        {
+            return $"{this.FullName} {this.BirthDay.ToShortDateString()} {this.Phone} {this.Email} {this.Address} {this.Debt}";
+        }
+    }
+}
